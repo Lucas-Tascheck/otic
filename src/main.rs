160dd8733ba0a0788af93925168a012fd1fe22cb
Lucas Tascheck <lucasguitascheck@gmail.com>
2025-09-22 12:41:34 -0300

    feat: fn comments

--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,20 @@ fn main() {
 
     // EX 1: Maximização de Função Algébrica 
     // valor otimo para X = 1.8904170901022
+    // minimize: c_max = 2.0; 
+    //           ((c_max - fx)/normalize).max(0.0)
+    // maximize:
+    //           c_min = 4.0;
+    //           ((fx + c_min)/normalize).max(0.0)
     run_exercicio1(pop_size, dim, gens, runs);
 
     //EX 2: Fabrica de Rádios
     //Maximização: genes = "1111111110" | fitness = 0.7571 | Lucro = 1029.68
     // 11111 = 24 st
     // 11110 = ~8 lx
-    //Lucro = 1040.0
+    // Lucro = 1040.0
+    // Objective function = (30.0 * st + 40.0 * lx) / 1360.0
+    // Penalty funciton = ((st + 2.0 * lx - 40.0) / 16.0).max(0.0)
+    // Fitness = Objective function - k * Penalty funciton
     //run_exercicio2(pop_size, dim, gens, runs, 1.0)
 }
